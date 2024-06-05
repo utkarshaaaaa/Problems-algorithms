5e22f179b47be6d4e76f1f85739d3e10792a14e9
utkarsh <vyasutkarsh11@gmail.com>
2024-06-05 17:22:23 +0530

    commit

---
+++ Binary_Search_problems/BinarySearch2_DArray.java
@@ -0,0 +1,45 @@
+package Binary_Search_problems;
+//time complexity O(n)
+
+import java.util.Arrays;
+
+public class BinarySearch2_DArray {
+    public static void main(String[] args) {
+
+        int[][] arr={
+            {10,20,30,40},
+            {15,25,35,45},
+            {28,29,35,49},
+            {33,34,38,50}
+        };
+
+        System.out.println(""+ " "+Arrays.toString(rowcol(arr, 35)));
+
+
+        
+    }
+public static int[] rowcol(int[][] matirx,int target){
+
+    int r=0;
+    int col=matirx.length-1;
+
+    while (r<matirx.length && col>0) {
+
+        if(matirx[r][col]==target){
+            return new int[] {r,col};
+        }
+        if(matirx[r][col] <target){
+            r++;
+
+        }else{
+            col--;
+        }
+       
+        
+    }
+    return new int[]{-1,-1};
+
+}
+
+
+}
---
+++ Binary_Search_problems/Sorted_2_d_Array.java
@@ -0,0 +1,11 @@
+package Binary_Search_problems;
+
+public class Sorted_2_d_Array{
+
+    public static void main(String[] args) {
+        
+        
+    }
+    
+    
+}
