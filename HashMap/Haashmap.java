2f5f0b6d0c182daf5bb2748570529b0a404d4c98
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-06-15 09:46:59 +0530

    commit

---
+++ HashMap/Haashmap.java
@@ -0,0 +1,15 @@
+package HashMap;
+import java.util.*;
+
+import String.strings;
+
+public class Haashmap {
+    public static void main(String[] args) {
+
+        HashMap<Integer,String> ids=new HashMap<>();
+        ids.put(1, "udv");
+        System.out.println(ids.get(1));
+        
+    }
+    
+}
