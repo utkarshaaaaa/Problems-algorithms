988ef6c3071aad2287cc2f1e8bdb2c25d938b026
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-06-24 14:09:51 +0530

    commit

--- randomDSAquestions/Missing_Repeating_No.java
+++ randomDSAquestions/Missing_Repeating_No.java
@@ -3,7 +3,39 @@ package randomDSAquestions;
 public class Missing_Repeating_No {
 
     public static void main(String[] args) {
+        {
+            int[] arr = { 7, 3, 4, 5, 5, 6, 2,2 };
+            int n = arr.length;
+            repeating(arr, n);
+        }
+
+
+
         
+    }
+
+    static void repeating(int[] arr,int n){
+
+        int[] temp = new int[n];
+        int repeat=-1;
+
+        for(int i=0;i<arr.length;i++){
+            temp[arr[i]-1]++;
+            if(temp[arr[i]-1]>1){
+                repeat=arr[i];
+                System.out.println(repeat);
+
+            }
+            
+            
+        }
+        for(int j=0;j<n;j++){
+            if(temp[j]==0){
+                System.out.println(temp[j]+1);
+                break;
+            }
+        }
+
     }
     
 }
