f89d1e5a6c87b57eb1650491b82d9c20d96eec19
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-06-24 12:17:12 +0530

    commit

--- HashMap/Hashmap.java
+++ HashMap/Hashmap.java
@@ -1,14 +1,22 @@
 package HashMap;
 import java.util.*;
+//time complexity 0(1)
+//key value pair
+//internally as array of linked list 
+//keys get into a hash function for giving output as the index of the array of linked list
+//lambda =n(number of nodes)/N(number of buckets or the index in array) if it is equal or greater than the threshold then the bucket size is increased
+
 
 import String.strings;
 
 public class Hashmap {
     public static void main(String[] args) {
+        int[] a={1,2};
 
         HashMap<Integer,String> ids=new HashMap<>();
         ids.put(1, "udv");
         System.out.println(ids.get(1));
+        System.out.println(ids.size());
         
     }
     
