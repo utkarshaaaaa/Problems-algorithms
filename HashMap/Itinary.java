a0cd2bf0a527a64c87d572e0b4320b21d3803ef3
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-06-30 14:21:12 +0530

    commit

--- HashMap/Itinary.java
+++ HashMap/Itinary.java
@@ -1,36 +1,30 @@
 package HashMap;
+
 import java.util.*;
 
 public class Itinary {
     public static void main(String[] args) {
-      HashMap<String,String> set=new HashMap<>();
-      set.put("chennai", "bengaluru");
-      set.put("mumbai", "delhi");
-      set.put("goa", "chennai");
-      set.put("delhi", "goa");
-      iti(set, "mumbai", "bengaluru");
+        HashMap<String, String> set = new HashMap<>();
+        set.put("chennai", "bengaluru");
+        set.put("mumbai", "delhi");
+        set.put("goa", "chennai");
+        set.put("delhi", "goa");
+        iti(set, "mumbai", "bengaluru");
 
-      
-        
     }
-    public static void iti(HashMap<String,String> map,String start,String end){
-        String a=start;
 
-        for(int i=0;i<map.keySet().size();i++){
-           
+    public static void iti(HashMap<String, String> map, String start, String end) {
+        String a = start;
+
+        for (int i = 0; i < map.keySet().size(); i++) {
+
             System.out.println(a);
-            String current=map.get(a);
-          
-            a=map.get(current);
-           
-          
+            String current = map.get(a);
 
-           
+            a = map.get(current);
+
         }
 
-
-      
-
     }
-   
+
 }
--- randomDSAquestions/CountPairsToFormCompleteDays.java
+++ randomDSAquestions/CountPairsToFormCompleteDays.java
@@ -1,42 +1,31 @@
 package randomDSAquestions;
+
 import java.util.*;
 
-public class CountPairsToFormCompleteDays  {
+public class CountPairsToFormCompleteDays {
     public static void main(String[] args) {
-        int[] arr={1,1,2,3,2,4,1,2,2,3,1};
-        int len=arr.length;
+        int[] arr = { 1, 1, 2, 3, 2, 4, 1, 2, 2, 3, 1 };
+        int len = arr.length;
 
-        count(arr,len);
-
-       
-        
+        count(arr, len);
 
     }
-    public static void count(int[] arr,int len){
 
-        HashMap<Integer,Integer> map=new HashMap<>();
-        int[] temp=new int[len];
-        
+    public static void count(int[] arr, int len) {
 
-        for(int i=0;i<len;i++){
-            if(map.containsKey(arr[i])){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int[] temp = new int[len];
 
-                map.put(arr[i], map.get(arr[i])+1);
-            }else{
+        for (int i = 0; i < len; i++) {
+            if (map.containsKey(arr[i])) {
+
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
                 map.put(arr[i], 1);
             }
 
-
         }
         System.out.println(map);
-        
 
-        
-
-
-       
-
-
-      
     }
 }
