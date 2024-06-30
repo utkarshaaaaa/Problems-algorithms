87a5ccd2bed4e40280215138736a51316d66a3e9
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-06-30 21:22:39 +0530

    commit

--- HashMap/SumOfTwoNumber.java
+++ HashMap/SumOfTwoNumber.java
@@ -4,36 +4,30 @@ import java.util.HashMap;
 
 public class SumOfTwoNumber {
     public static void main(String[] args) {
-        int[] arr1={2,7,11,15};
-       
+        int[] arr1 = { 2, 7, 11, 15 };
+
         SumNumbers(arr1, 13);
-        
+
     }
-     public static void SumNumbers(int[] arr1,int target){
 
-        HashMap<Integer,Integer> map=new HashMap<>();
+    public static void SumNumbers(int[] arr1, int target) {
 
-        for(int i=0;i<arr1.length;i++){
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-            int toTarget=target-arr1[i];
+        for (int i = 0; i < arr1.length; i++) {
+
+            int toTarget = target - arr1[i];
 
             map.put(toTarget, i);
-           
-            if(map.containsKey(toTarget)){
+
+            if (map.containsKey(toTarget)) {
                 System.out.println(arr1[map.get(toTarget)]);
                 System.out.println(toTarget);
                 break;
             }
 
-          
-
         }
- 
 
-
-       
-        
     }
-    
-    
+
 }
