81611ae0070ea458cae101b9fd10aaa95a717e0b
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-08-01 15:46:47 +0530

    commit

--- oop/opps1.java
+++ oop/opps1.java
@@ -6,21 +6,31 @@ package oop;
 //student stu1   =      new student();
 // compile time        run time 
 //when the memory is allocated while in run time is called dynamic memory allocation  
+//comstructor define what happens when a object is created ,it is a special functions which runs when we allocate an object and it allocates 
+//some variables
 
+//"this" keyword 
+ class Inneropps1 {
+    int value;
+    public Inneropps1(int val){
+        this.value=val;
+
+
+    }
+
+    int[] Narr= new int[9];
+    
+
+    
+}
 public class opps1 {
 
-    public static class Inneropps1 {
-
-        int[] Narr= new int[9];
-        
-    
-        
-    }
+   
     public static void main(String[] args) {
+        
 
-        Inneropps1 i=new Inneropps1();
-        i.Narr[0]=1;
-        System.out.println(i.Narr.toString());
+        Inneropps1 i=new Inneropps1(20);
+        System.out.println(i.value);
          
 
     }
