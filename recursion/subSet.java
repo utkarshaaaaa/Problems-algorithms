3a53cd1887b21b68eb0f61a804b71a3c695ce7e0
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-08-16 21:24:35 +0530

    commit

--- recursion/subSet.java
+++ recursion/subSet.java
@@ -1,7 +1,19 @@
 package recursion;
 
 public class subSet {
+    static void sub(String s,String modified){
+        if(s.isEmpty()){
+            System.out.println(modified);
+            return ;
+        }
+        char a=s.charAt(0);
+        sub(s.substring(1),modified + a);
+        sub(s.substring(1), modified);
+    }
     public static void main(String[] args) {
+        sub("abcd","");
+
+
         
     }
     
