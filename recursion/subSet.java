9132a6d2e222d70292ca5db1bd93b5b3c8713239
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-08-17 10:53:48 +0530

    commit

--- recursion/subSet.java
+++ recursion/subSet.java
@@ -4,7 +4,7 @@ public class subSet {
     static void sub(String s,String modified){
         if(s.isEmpty()){
             System.out.println(modified);
-            return ;
+            return;
         }
         char a=s.charAt(0);
         sub(s.substring(1),modified + a);
