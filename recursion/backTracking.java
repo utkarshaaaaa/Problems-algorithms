c8accd8431302c048b82cd8274d9b0f98afb53cc
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-03 16:39:18 +0530

    commit

--- recursion/backTracking.java
+++ recursion/backTracking.java
@@ -1,7 +1,18 @@
 package recursion;
 
 public class backTracking {
+    static int numberOfPath(int row,int column){
+        if(row ==1|| column==1){
+            return 1;
+
+        }
+        int right=numberOfPath(row-1, column);
+        int dowm=numberOfPath(row, column-1);
+       
+        return right + dowm;
+    }
     public static void main(String[] args) {
+        System.out.println(numberOfPath(3, 3));
         
     }
     
