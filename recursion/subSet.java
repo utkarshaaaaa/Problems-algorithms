5eafde215a578f60a27de705ab2b5ea1a91aab42
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-04 15:47:02 +0530

    commit

--- recursion/subSet.java
+++ recursion/subSet.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.ArrayList;
+
 public class subSet {
     static void sub(String s,String modified){
         if(s.isEmpty()){
@@ -10,8 +12,23 @@ public class subSet {
         sub(s.substring(1),modified + a);
         sub(s.substring(1), modified);
     }
+    static ArrayList<String> retSub(String p,String up){
+        if(up.length()==0){
+            ArrayList<String> list=new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char f=up.charAt(0);
+        ArrayList<String> left=retSub(p +f, up.substring(1));
+        ArrayList<String> right=retSub(p, up.substring(1));
+
+        left.addAll(right);
+        return left;
+    }
     public static void main(String[] args) {
         sub("abcd","");
+
+        System.out.println(retSub("", "abc"));
 
 
         
