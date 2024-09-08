4a1ac50a86c2b18af23e40c2d2e69479c3c68f47
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-08 15:46:20 +0530

    commit

---
+++ recursion/pathDirection.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class pathDirection {
+    static void path(String p,int s,int e){
+        if(s==1&& e==1){
+            System.out.println(p);
+            return;
+        }
+        if(s>=1){
+            path(p+'R', s-1, e);
+        }
+        if(e>=1){
+            path(p+'B', s, e-1);
+        }
+
+    }
+    public static void main(String[] args) {
+        path("", 3, 3);
+        
+    }
+    
+}
