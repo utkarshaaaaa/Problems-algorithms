97262a1ff76767a95f8212a9162ce7086660fe7a
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-16 00:20:12 +0530

    commit

--- heapDataStructure/heap.java
+++ heapDataStructure/heap.java
@@ -1,11 +1,24 @@
 package heapDataStructure;
-//heapes aee complete Binart tree
+//heapes are complete Binart tree
 //Min heap :- value of root node nodes is less than or equal to its child nodes
 //Hax heap :- value of root node nodes is greater than or equal to its child nodes
 //Time complexity- log2(N) for searching o(n)
 //we can move from child to parent and from parent to child 
 //heapify:-the process of rearranging the heap by recursively comparing aparent node with the child node
+//heapify is a angorithm which converts any array into heap 
 public class heap {
+    int heap[];
+    int heapSize;
+    int capacity;
+    heap(int arr[]){
+        heap=arr;
+        heapSize=arr.length;
+        capacity=arr.length;
+
+    }
+    static void heapify(){
+        
+    }
     public static void main(String[] args) {
         
     }
