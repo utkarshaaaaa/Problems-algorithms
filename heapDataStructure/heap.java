8db692666b8e2dd74f5f76fb76095cdd5b23815b
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-15 12:29:25 +0530

    commit

--- heapDataStructure/heap.java
+++ heapDataStructure/heap.java
@@ -1,5 +1,8 @@
 package heapDataStructure;
-
+//heapes aee complete Binart tree
+//Min heap :- value of root node nodes is less than or equal to its child nodes
+//Hax heap :- value of root node nodes is greater than or equal to its child nodes
+//Time complexity- log2(N) for searching o(n)
 public class heap {
     public static void main(String[] args) {
         
