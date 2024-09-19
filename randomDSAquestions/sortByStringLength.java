2a9fb3adcbc0e870a4b7ddf3f5bd777705afc573
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-20 01:18:16 +0530

    commit

--- randomDSAquestions/sortByStringLength.java
+++ randomDSAquestions/sortByStringLength.java
@@ -5,20 +5,18 @@ import java.util.Comparator;
 
 public class sortByStringLength {
     public static void main(String[] args) {
-        String[] at= new String[]{"dedw","niiininxw","xwuyb","wnxxw","iubuqb","iu"};
+       
         ArrayList<String> ar= new ArrayList<>();
-        ar.add("dedw");
-        ar.add("niiininxw");
-        ar.add("xwuyb");
-        ar.add("wnxxw");
+        ar.add("87");
+        ar.add("98");
+        ar.add("23");
+        ar.add("95");
 
         Comparator<String> com= new Comparator<String>() {
             public int compare(String a,String b){
-                if(a.length()< b.length()){
-                    return 1;
-                }else{
-                    return -1;
-                }
+                String f=a+b;
+                String s=b+a;
+                return s.compareTo(f);
 
             }
         };
