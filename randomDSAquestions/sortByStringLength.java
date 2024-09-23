c1074aefe07be8c0b135f35735630755b5ab0502
utkarshaaaaa <vyasutkarsh11@gmail.com>
2024-09-23 20:55:52 +0530

    commit

--- randomDSAquestions/sortByStringLength.java
+++ randomDSAquestions/sortByStringLength.java
@@ -4,6 +4,17 @@ import java.util.*;
 import java.util.Comparator;
 
 public class sortByStringLength {
+    static void sb(){
+        StringBuilder a= new StringBuilder();
+        a.append("01234");
+        for( int i=0;i<a.length();i++){
+            if(i%2!=0){
+                a.deleteCharAt(i);
+            }
+        }
+        System.out.println(a);
+
+    }
     public static void main(String[] args) {
        
         ArrayList<String> ar= new ArrayList<>();
@@ -22,6 +33,7 @@ public class sortByStringLength {
         };
         Collections.sort(ar,com);
         System.out.println(ar);
+        sb();
     }
     
 }
